fa5b627115b7688bf8eab7b99bf25d289ba5c90b
Dove6 <4943032+Dove6@users.noreply.github.com>
2025-11-09 18:05:33 +0100

    Move to pragmatic-drag-and-drop, using the Board example

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@atlaskit/css-reset';
+import AppProvider from '@atlaskit/app-provider';
+
+import Example from './example';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <Example />
+    </AppProvider>
   </StrictMode>,
-)
+);
