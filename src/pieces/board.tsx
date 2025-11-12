580b7091abe0d2aa6550639d0aff045de2bbd324
Dove6 <4943032+Dove6@users.noreply.github.com>
2025-11-12 03:36:30 +0100

    Make image icons square, make images column bigger

--- src/pieces/board.tsx
+++ src/pieces/board.tsx
@@ -15,7 +15,7 @@ const boardStyles = xcss({
 	justifyContent: 'center',
 	gap: 'space.200',
 	flexDirection: 'row',
-	height: '480px',
+	height: '720px',
 });
 
 const Board = forwardRef<HTMLDivElement, BoardProps>(({ children }: BoardProps, ref) => {
