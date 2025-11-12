bb2d43be6fd8e66e67ab179159fb3a54ae475d55
Dove6 <4943032+Dove6@users.noreply.github.com>
2025-11-12 01:52:12 +0100

    Match version of react and react-dom with their types counterparts

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
 import AppProvider from '@atlaskit/app-provider';
